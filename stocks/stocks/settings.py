e34ed152f76857dfef53cd372a7986f40e5c8e89
Tyler Yep <tyep@cs.stanford.edu>
2021-05-18 22:26:34 -0700

    add rest_framework

--- stocks/stocks/settings.py
+++ stocks/stocks/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -63,9 +64,9 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ],
+            ]
         },
-    },
+    }
 ]
 
 WSGI_APPLICATION = "stocks.wsgi.application"
